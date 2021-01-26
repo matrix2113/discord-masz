45b2a39ae6db13ebeab22a0a1580349bae12ed0e
Andy <49132750+matrix2113@users.noreply.github.com>
2021-01-26 13:18:02 -0500

    Create prefix.py

---
+++ discordbot/src/data/prefix.py
@@ -0,0 +1,16 @@
+import discord
+import os 
+
+from discord.ext import commands
+
+default_prefix = "$"
+
+async def get_prefix(ctx):
+    if ctx.guild == None:
+        return default_prefix
+    else:
+        prefix = os.getenv("BOT_PREFIX", "$")
+        if str(prefix).strip() != "":
+            return "$"
+        else:
+            return "$",
